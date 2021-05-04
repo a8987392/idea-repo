a5acc1561d057441845721e3fbbeadf60325c49d
admin <zhangsan@qq.com>
2021-05-04 20:16:41 +0800

    add

---
+++ src/hello/HELLO.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class HELLO {
+    public static void main(String args []){
+
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+
+
+    }
+
+
+
+}
